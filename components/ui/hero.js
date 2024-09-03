6096caa84d413c8c72ce6308d1a2b9e9cd267a72
Abhishek <graciadeveloper@gmail.com>
2024-09-03 18:24:20 +0530

    ..

--- components/ui/hero.js
+++ components/ui/hero.js
@@ -15,7 +15,16 @@ const Hero = () => {
         <source src="/hero/heroLargeDevice.webm" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-
+      <video 
+        className="video-mobile" 
+        autoPlay 
+        loop 
+        muted 
+        playsInline
+      >
+        <source src="hero/heroMobile.webm" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       {/* <div className="hero-content">
         <h1>Welcome to Our Website</h1>
         <p>Your journey to sustainability starts here.</p>
