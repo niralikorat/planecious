54bb6cec29abbd2b47dc15e075326857a3a37c9a
Abhishek <graciadeveloper@gmail.com>
2024-09-07 15:07:06 +0530

    ..

--- components/ui/hero.js
+++ components/ui/hero.js
@@ -1,30 +1,31 @@
-// Hero.js
 import React from 'react';
 
 const Hero = () => {
   return (
     <div className="hero-container">
       {/* Desktop video */}
-      <video 
-        className="video-desktop" 
-        autoPlay 
-        loop 
-        muted 
-        playsInline 
+      <video
+        className="video-desktop"
+        autoPlay
+        loop
+        muted
+        playsInline
         preload="auto"
+        webkit-playsinline="true" /* iOS-specific attribute */
       >
         <source src="/hero/heroLargeDevice.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      
+
       {/* Mobile video */}
-      <video 
-        className="video-mobile" 
-        autoPlay 
-        loop 
-        muted 
-        playsInline 
+      <video
+        className="video-mobile"
+        autoPlay
+        loop
+        muted
+        playsInline
         preload="auto"
+        webkit-playsinline="true" /* iOS-specific attribute */
       >
         <source src="/hero/heroMobile.mp4" type="video/mp4" />
         Your browser does not support the video tag.
