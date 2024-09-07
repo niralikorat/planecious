e48bc9a5fd57b420ac6d8d1708c6669d601a31ab
Abhishek <graciadeveloper@gmail.com>
2024-09-07 14:48:53 +0530

    ..

--- components/ui/hero.js
+++ components/ui/hero.js
@@ -1,38 +1,34 @@
 // Hero.js
 import React from 'react';
-import Navbar from '../navBar';
-
 
 const Hero = () => {
   return (
     <div className="hero-container">
-    {/* <Navbar/> */}
+      {/* Desktop video */}
       <video 
         className="video-desktop" 
         autoPlay 
         loop 
         muted 
-        playsInline
+        playsInline 
         preload="auto"
       >
         <source src="/hero/heroLargeDevice.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+      
+      {/* Mobile video */}
       <video 
         className="video-mobile" 
         autoPlay 
         loop 
         muted 
-        playsInline
+        playsInline 
         preload="auto"
       >
-        <source src="hero/heroMobile.mp4" type="video/mp4" />
+        <source src="/hero/heroMobile.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      {/* <div className="hero-content">
-        <h1>Welcome to Our Website</h1>
-        <p>Your journey to sustainability starts here.</p>
-      </div> */}
     </div>
   );
 };
