68c6925ade683c7499215c1f35ad35949f0f9b6b
Abhishek <graciadeveloper@gmail.com>
2024-09-09 15:01:35 +0530

    ..

--- components/ui/section2.js
+++ components/ui/section2.js
@@ -1,38 +1,60 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { useEffect, useRef } from 'react';
-import CarouselComponent from '../carousel';
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { useEffect, useRef } from "react";
+import CarouselComponent from "../carousel";
 
 export default function Section2() {
-
   const section2Data = [
-    { imgUrl: 'aboutIcon/Icons-01.png', title: 'People', detail: 'Assum aperiris his eam mea', button: '' },
-    { imgUrl: 'aboutIcon/Icons-02.png', title: 'Planet', detail: 'Assum aperiris his eam mea', button: '' },
-    { imgUrl: 'aboutIcon/Icons-04.png', title: 'Community', detail: 'Assum aperoros his eam mea', button: '' },
-    { imgUrl: 'aboutIcon/Icons-03.png', title: 'Sustainability', detail: 'Assum aperiris his eam mea', button: '' }
+    {
+      imgUrl: "aboutIcon/Icons-01.png",
+      title: "People",
+      detail: "Assum aperiris his eam mea",
+      button: "",
+    },
+    {
+      imgUrl: "aboutIcon/Icons-02.png",
+      title: "Planet",
+      detail: "Assum aperiris his eam mea",
+      button: "",
+    },
+    {
+      imgUrl: "aboutIcon/Icons-04.png",
+      title: "Community",
+      detail: "Assum aperoros his eam mea",
+      button: "",
+    },
+    {
+      imgUrl: "aboutIcon/Icons-03.png",
+      title: "Sustainability",
+      detail: "Assum aperiris his eam mea",
+      button: "",
+    },
   ];
 
   const videoData = [
     {
-      url: 'https://www.youtube.com/embed/6Reaa3gH9dc?si=v6TY2XDLDwBxLm9Z',
-      category: 'SUSTAINABILITY',
-      heading: 'Sustainability and DEI Expert',
-      paragraph: 'Welcome to Planecious, the ultimate platform for individuals passionate about combating climate change and building a sustainable future.',
+      url: "https://www.youtube.com/embed/6Reaa3gH9dc?si=v6TY2XDLDwBxLm9Z",
+      category: "SUSTAINABILITY",
+      heading: "Sustainability and DEI Expert",
+      paragraph:
+        "Welcome to Planecious, the ultimate platform for individuals passionate about combating climate change and building a sustainable future.",
     },
     {
-      url: 'https://www.youtube.com/embed/WJUo697afdc?si=SkfYiIbJXx6KaTw3',
-      category: 'NATURE',
-      heading: 'ESG Reporting & Rating Consultant',
-      paragraph: 'Welcome to Planecious, the ultimate platform for individuals passionate about combating climate change and building a sustainable future.',
+      url: "https://www.youtube.com/embed/WJUo697afdc?si=SkfYiIbJXx6KaTw3",
+      category: "NATURE",
+      heading: "ESG Reporting & Rating Consultant",
+      paragraph:
+        "Welcome to Planecious, the ultimate platform for individuals passionate about combating climate change and building a sustainable future.",
     },
     {
-      url: 'https://www.youtube.com/embed/PatHQFStlZI?si=3PY9bvDe4ghwlx0J',
-      category: 'CARBON',
-      heading: 'Introducing Planecious',
-      paragraph: 'Welcome to Planecious, the ultimate platform for individuals passionate about combating climate change and building a sustainable future.',
+      url: "https://www.youtube.com/embed/PatHQFStlZI?si=3PY9bvDe4ghwlx0J",
+      category: "CARBON",
+      heading: "Introducing Planecious",
+      paragraph:
+        "Welcome to Planecious, the ultimate platform for individuals passionate about combating climate change and building a sustainable future.",
     },
   ];
   const animatedRefs = useRef([]); // Array of refs
@@ -42,7 +64,7 @@ export default function Section2() {
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            entry.target.classList.add('aboutSecIcons');
+            entry.target.classList.add("aboutSecIcons");
             observer.unobserve(entry.target); // Stop observing after animation is triggered
           }
         });
@@ -63,30 +85,42 @@ export default function Section2() {
 
   const communityPosts = [
     {
-      url: 'https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7226182797361606657',
-      category: 'COMMUNITY',
-      heading: 'Volunteer in Our Community',
-      paragraph: 'Join our community and make a difference!',
-    }
+      url: "https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7226182797361606657",
+      category: "COMMUNITY",
+      heading: "Volunteer in Our Community",
+      paragraph: "Join our community and make a difference!",
+    },
   ];
 
   return (
-    <div className='flex flex-col'>
-      <div className='section2 py-24 px-4 md:px-12 flex flex-col text-black bg-opacity-50' id='about'>
-        <div className='flex flex-col gap-8 justify-center items-center'>
-          <p className='example text-md text-center p-4 -rotate-12'>ABOUT US</p>
-          <p className='max-w-2xl text-center text-sm'>We represent a community of optimists who strive to make a difference. Planecious turns
-everyday environmental action and influencing into effortless ventures. Our initiative makes
-living more sustainably easier, enabling any person to create an impact and build towards the
-
-future the planet deserves.</p>
-          <p className='text-5xl font-semibold text-center'>An initiative for change by aligning</p>
-         
+    <div className="flex flex-col">
+      <div
+        className="section2 py-24 px-4 md:px-12 flex flex-col text-black bg-opacity-50"
+        id="about"
+      >
+        <div className="flex flex-col gap-8 justify-center items-center">
+          <p className="example text-md text-center p-4 -rotate-12">ABOUT US</p>
+          <p className="max-w-2xl text-center text-sm">
+            We represent a community of optimists who strive to make a
+            difference. Planecious turns everyday environmental action and
+            influencing into effortless ventures. Our initiative makes living
+            more sustainably easier, enabling any person to create an impact and
+            build towards the future the planet deserves.
+          </p>
+          <p className="text-5xl font-semibold text-center">
+            An initiative for change by aligning
+          </p>
         </div>
-        <div  className='flex flex-wrap gap-16 lg:gap-20 xl:gap-28 justify-center items-center ' >
+        <div className="flex flex-wrap gap-16 lg:gap-20 xl:gap-28 justify-center items-center ">
           {section2Data.map((data, index) => (
-            <div  key={index} className={`${index % 2 !== 0 && 'md:mt-40'} mt-12 flex flex-col justify-center items-center gap-4 animated-box`}     ref={(el) => (animatedRefs.current[index] = el)} >
-              <img src={data.imgUrl} className='w-[120px]' alt={data.title} />
+            <div
+              key={index}
+              className={`${
+                index % 2 !== 0 && "md:mt-40"
+              } mt-12 flex flex-col justify-center items-center gap-4 animated-box`}
+              ref={(el) => (animatedRefs.current[index] = el)}
+            >
+              <img src={data.imgUrl} className="w-[120px]" alt={data.title} />
               <p>{data.title}</p>
               {/* <p>{data.detail}</p> */}
               {/* <Link className='underline' href={'/'}>TAKE ACTION</Link> */}
@@ -95,52 +129,87 @@ future the planet deserves.</p>
         </div>
       </div>
 
-      <div className='flex flex-col md:flex-row'>
-        <div className='flex flex-row items-center bgPrimary w-full lg:w-2/5'>
-          <div className='flex flex-col text-white justify-center items-start gap-4 px-10 lg:px-20 py-32 '>
-            <p className='example -rotate-6 text-md'>E-book</p>
-            <p className=' text-3xl lg:text-4xl font-semibold '>A Guide to Sustainable Marketing in 2024</p>
-            <Link className='bg-white px-4 py-3 lg:px-8 lg:py-4 rounded-3xl text-xs lg:text-sm font-semibold text-black mt-2 hover:scale-105' href={'ebook.pdf'} target="_blank">READ NOW</Link>
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-row items-center bgPrimary w-full lg:w-2/5">
+          <div className="flex flex-col text-white justify-center items-start gap-4 px-10 lg:px-20 py-32 ">
+            <p className="example -rotate-6 text-md">E-book</p>
+            <p className=" text-3xl lg:text-4xl font-semibold ">
+              A Guide to Sustainable Marketing in 2024
+            </p>
+            <Link
+              className="bg-white px-4 py-3 lg:px-8 lg:py-4 rounded-3xl text-xs lg:text-sm font-semibold text-black mt-2 hover:scale-105"
+              href={"ebook.pdf"}
+              target="_blank"
+            >
+              READ NOW
+            </Link>
           </div>
-          <div className='triangle-right'></div>
+          <div className="triangle-right"></div>
         </div>
-        <img src={'/ebookBanner.jpg'} className='w-full lg:w-3/5' alt='img' />
+        <img src={"/ebookBanner.jpg"} className="w-full lg:w-3/5" alt="img" />
       </div>
 
-      <div className='flex flex-col md:flex-row bgPodcast p-4 md:px-20 py-32 justify-between gap-10' id='walkTheTalk'>
-        <div className='flex flex-col gap-4 items-start justify-center  '>
-          <p className='example -rotate-6 text-md'>Latest</p>
-          <p className='font-semibold text-4xl text-black'>Walk The Talk</p>
-          <p className='max-w-md'>Discover people changing the face of our planet for good.
-          Tune in and listen to their sure-to-inspire stories.</p>
+      <div
+        className="flex flex-col md:flex-row bgPodcast p-4 md:px-20 py-32 justify-between gap-10"
+        id="walkTheTalk"
+      >
+        <div className="flex flex-col gap-4 items-start justify-center  ">
+          <p className="example -rotate-6 text-md">Latest</p>
+          <p className="font-semibold text-4xl text-black">Walk The Talk</p>
+          <p className="max-w-md">
+            Discover people changing the face of our planet for good. Tune in
+            and listen to their sure-to-inspire stories.
+          </p>
           {/* <button className='bgPrimary px-10 py-4 rounded-3xl text-sm font-semibold text-white mt-2'>VIEW MORE</button> */}
-          
         </div>
         <CarouselComponent videoList={videoData} autoPlay={true} />
       </div>
 
-      <div className='bgSection3 flex flex-col py-32 md:px-20 gap-4 items-center text-white' id='JoinUs'>
-        <p className='example -rotate-6 text-md'>COMMUNITY</p>
-        <p className='font-semibold text-4xl text-white  text-center'>Action and Change starts from here,<br></br>
-        Be a part of Planecious.</p>
+      <div
+        className="bgSection3 flex flex-col py-32 md:px-20 gap-4 items-center text-white"
+        id="JoinUs"
+      >
+        <p className="example -rotate-6 text-md">COMMUNITY</p>
+        <p className="font-semibold text-4xl text-white  text-center">
+          Action and Change starts from here,<br></br>
+          Be a part of Planecious.
+        </p>
         {/* <CarouselComponent videoList={communityPosts} autoPlay={true} /> */}
         {/* <button className='bgPrimary px-10 py-4 rounded-3xl text-sm font-semibold text-white mt-2'>JOIN COMMUNITY</button> */}
-        <Link className='bgPrimary px-6 py-4 rounded-3xl text-xs font-semibold text-white mt-2 hover:scale-105' href={'https://www.linkedin.com/groups/8147680/'}>JOIN HERE</Link>
+        <Link
+          className="bgPrimary px-6 py-4 rounded-3xl text-xs font-semibold text-white mt-2 hover:scale-105"
+          href={"https://www.linkedin.com/groups/8147680/"}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          JOIN HERE
+        </Link>
       </div>
-      <section className='flex flex-col lg:flex-row justify-evenly h-full lg:h-[70vh]' id='Initiatives'>
-        <div className='flex flex-col w-full'>
-          <div className='flex flex-col gap-4 py-10 px-10 lg:px-20 justify-center h-full '>
-          <p className='font-semibold text-4xl text-black   text-start'>Key to a More Eco-friendly world !!!</p>
-         
+      <section
+        className="flex flex-col lg:flex-row justify-evenly h-full lg:h-[70vh]"
+        id="Initiatives"
+      >
+        <div className="flex flex-col w-full">
+          <div className="flex flex-col gap-4 py-10 px-10 lg:px-20 justify-center h-full ">
+            <p className="font-semibold text-4xl text-black   text-start">
+              Key to a More Eco-friendly world !!!
+            </p>
           </div>
-          <div className='flex flex-col lg:flex-row w-screen lg:w-auto lg:h-[46vh]'>
-          <img src='dekoratus.jpg' className='w-full lg:w-1/2'/>
-          <div className='flex flex-col gap-4 justify-center items-start bgSection3card px-10 lg:px-20 py-20 lg:py-8 w-full lg:w-1/2 '>
-          <p className='font-semibold text-2xl xl:text-3xl text-black  text-start gradientText max-w-md'>A healthier planet, happier people,Our mission in action.</p>
-          <Link className='underline text-sm' href={'https://dekoratus.com/'} target="_blank">VISIT US</Link>
+          <div className="flex flex-col lg:flex-row w-screen lg:w-auto lg:h-[46vh]">
+            <img src="dekoratus.jpg" className="w-full lg:w-1/2" />
+            <div className="flex flex-col gap-4 justify-center items-start bgSection3card px-10 lg:px-20 py-20 lg:py-8 w-full lg:w-1/2 ">
+              <p className="font-semibold text-2xl xl:text-3xl text-black  text-start gradientText max-w-md">
+                A healthier planet, happier people,Our mission in action.
+              </p>
+              <Link
+                className="underline text-sm"
+                href={"https://dekoratus.com/"}
+                target="_blank"
+              >
+                VISIT US
+              </Link>
+            </div>
           </div>
-          </div>
-       
         </div>
 
         {/* <div className='flex flex-col w-auto '>
@@ -150,15 +219,21 @@ future the planet deserves.</p>
         
 
         </div> */}
-        <div className='flex flex-col  h-[70vh]'>
-          <img src='/graciaBanner.jpg' className='h-1/2 '/ >
-          <div className='flex flex-col gap-16 justify-center items-start  py-10  px-10 lg:px-20 bg-orange-400 h-1/2'>
-          <p className='font-semibold text-2xl 2xl:text-3xl text-white  max-w-lg text-start'>An initiative to empower brands purposefully.</p>
-          <div className='flex flex-col gap-2 text-white'>
-            {/* <p className='example'>Abhishek Raj</p> */}
-            <Link href={'https://graciamarcom.com/'} className='text-md hover:underline'>GRACIA MARCOM</Link>
-          </div>
-      
+        <div className="flex flex-col  h-[70vh]">
+          <img src="/graciaBanner.jpg" className="h-1/2 " />
+          <div className="flex flex-col gap-16 justify-center items-start  py-10  px-10 lg:px-20 bg-orange-400 h-1/2">
+            <p className="font-semibold text-2xl 2xl:text-3xl text-white  max-w-lg text-start">
+              An initiative to empower brands purposefully.
+            </p>
+            <div className="flex flex-col gap-2 text-white">
+              {/* <p className='example'>Abhishek Raj</p> */}
+              <Link
+                href={"https://graciamarcom.com/"}
+                className="text-md hover:underline"
+              >
+                GRACIA MARCOM
+              </Link>
+            </div>
           </div>
         </div>
       </section>
